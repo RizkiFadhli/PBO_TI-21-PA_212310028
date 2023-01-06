ba6c00b64f4926a611813e83dc20247cd6448086
RizkiFadhli <113278653+RizkiFadhli@users.noreply.github.com>
2023-01-06 22:37:01 +0700

    Add files via upload

---
+++ PBO-212310028-PRAKTIKUM-9/src/main/java/com/ibik/pbo/praktikum/Auth/RegisterDAO.java
@@ -0,0 +1,29 @@
+package com.ibik.pbo.praktikum.Auth;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import com.ibik.pbo.praktikum.DBConnection;
+
+public class RegisterDAO {
+	
+	private String queryRegister = "insert into admin (email, password, fullname, phone, gender, citizenship)"
+			+ "values (?, ?, ?, ?, ?, ?,)";
+	
+	public void savedRegister(Admin admin) throws Exception {
+		Connection c = new DBConnection().connect();
+		
+		PreparedStatement psInsert = c.prepareStatement(queryRegister);
+		
+		psInsert.setString(1, admin.getEmail());
+		psInsert.setString(2, admin.getPassword());
+		psInsert.setString(3, admin.getFullname());
+		psInsert.setString(4, admin.getPhone());
+		psInsert.setString(5, admin.getGender());
+		psInsert.setString(6, admin.getCitizenship());
+		
+		psInsert.executeUpdate();
+		
+		c.close();
+	}
+}
